a5353e5f5fd3879794873b6d97fd4bc54c6eb171
vijay9288 <148909154+vijay9288@users.noreply.github.com>
2023-10-31 11:38:15 -0400

    Update streamlit.py
    fixed the error

--- streamlit.py
+++ streamlit.py
@@ -15,9 +15,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 #Added the file
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.dataframe(my_fruit_list)
 
